4a324a742f541476607ba73a514bc55ef00e9e9a
Arron Nestor <arronnestor@arrons-mbp.mynetworksettings.com>
2022-05-19 13:42:25 -0400

    added test files and version variable

--- src/action.js
+++ src/action.js
@@ -1,5 +1,45 @@
-async function run(){
-    console.log('Hello world!')
+var path = require('path'),
+fs = require('fs');
+
+const yaml = require('js-yaml')
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+function run(){
+
+    let returnCatalog = []
+    let modules = []
+
+    function fromDir(startPath, filter, callback) {
+    
+        if (!fs.existsSync(startPath)) {
+            console.log("no dir ", startPath);
+            return;
+        }
+    
+        var files = fs.readdirSync(startPath);
+        for (var i = 0; i < files.length; i++) {
+            var filename = path.join(startPath, files[i]);
+            var stat = fs.lstatSync(filename);
+            if (stat.isDirectory()) {
+                fromDir(filename, filter, callback); //recurse
+            } else if (filter.test(filename)) callback(filename);
+        };
+    };
+    
+    fromDir('./', /self-service-catalog\.yaml$/, function(filename) {
+        let fileContents = fs.readFileSync(filename, 'utf8');
+        let data = yaml.load(fileContents)
+        if(!data) return
+        modules = [...modules, data]
+    });
+
+    returnCatalog = {
+        'version': `${core.getInput('version')}`,
+        modules
+    }
+    console.log(returnCatalog)
+
 }
 
 run()
